f89e839c19e14db73ab7e6e86ddf898748f65306
Yan Wang <yan.wang@agon-solutions.de>
2018-04-19 11:45:11 +0200

    update

--- src/pages/addaccident/addaccident.ts
+++ src/pages/addaccident/addaccident.ts
@@ -26,7 +26,7 @@ export class AddaccidentPage {
   subscription: Subscription;
   // quality 20 is enough 3MB size photo will be reduced to about 100KB
   cameraOptions: CameraOptions = {
-    quality: 20,
+    quality: 15,
     destinationType: this.camera.DestinationType.DATA_URL,
     encodingType: this.camera.EncodingType.JPEG,
     mediaType: this.camera.MediaType.PICTURE
